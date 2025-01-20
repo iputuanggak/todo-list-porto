bee9900228e953726423a9c9ecc6abb6a2375b37
putuangga <iputuanggakrisna@gmail.com>
2025-01-20 15:16:54 +0700

    add: github-link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,20 @@
 
 import AddTask from "@/components/AddTask";
 import CardList from "@/components/CardList";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="my-4 max-w-[720px] mx-auto px-4">
+    <main className="mx-auto my-4 max-w-[720px] px-4">
+      <Link
+        href="https://github.com/iputuanggak/todo-list-porto"
+        className="mb-2 flex gap-1"
+      >
+        <img src="/img/github-mark.png" alt="" className="size-4 self-center" />
+        <p className="text-sm underline md:text-base">
+          https://github.com/iputuanggak/todo-list-porto
+        </p>
+      </Link>
       <CardList />
       <AddTask />
     </main>
